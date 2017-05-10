30703b13f9f8cdea7896ac77259fd7cf90f6374a
Andrew Hastings <droo.hastings@gmail.com>
2017-05-10 14:40:28 -0500

    cleaned up script.js

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,28 +1,32 @@
 var myApp=angular.module( 'myApp', [] );
+
 myApp.controller( 'WhereMyPeeps', [ '$http', function( $http ){
-var vm = this;
-vm.addRecord = function(){
-var objectToSend ={
-name= vm.nameIn,
-location= vm.locationIn,
-};
-$http({
-method: 'POST',
-url: '/testPost',
-data: objectToSend
-});
-vm.nameIn ='';
-vm.locationIn='';
-};
-vm.getRecords = function(){
-$.http({
-method: 'GET',
-url: '/getRecords',
-}).then( function( response ){
-vm.allTheRecords = response;
-console.log( vm.allTheRecords );
-}), function myError( response ){
-console.log( response.statusText );
-};
-};
-}]);
+  var vm = this;
+  //add record function
+  vm.addRecord = function(){
+  var objectToSend ={
+    name: vm.nameIn,
+    location: vm.locationIn
+  };//end objectToSend
+  $http({
+    method: 'POST',
+    url: '/testPost',
+    data: objectToSend
+  });//end POST
+  //empty fields
+    vm.nameIn ='';
+    vm.locationIn='';
+  };// end addRecord
+
+  vm.getRecords = function(){
+    $.http({
+      method: 'GET',
+      url: '/getRecords',
+    }).then( function( response ){
+      vm.allTheRecords = response;
+      console.log( vm.allTheRecords );
+    }), function myError( response ){
+      console.log( response.statusText );
+    }); //end error
+  };//end getRecords
+}]);//end controller
